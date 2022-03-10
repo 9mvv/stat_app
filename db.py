6c6f22176104f2bccb4c1a7ee38a8514fc0e796c
Adik <9vadim@gmail.com>
2022-03-10 22:56:08 +0300

    fix problems2

--- db.py
+++ db.py
@@ -6,11 +6,10 @@ mysql_host = environ['DB_SERVER']
 mysql_user = environ['DB_USER']
 mysql_passwd = environ['DB_PASS']
 mysql_db = environ['DB_NAME']
-conn_string = "host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode= True"
 
 
 def add_record(sql_insert, data):
-    db = MySQLdb.connect(conn_string)
+    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True)
     cursor = db.cursor()
     cursor.execute(sql_insert, data)
     db.commit()
@@ -20,7 +19,7 @@ def add_record(sql_insert, data):
 
 def set_opt(opt_name, opt_val):
     log.info('Запись опции {} в БД.'.format(opt_name))
-    db = MySQLdb.connect(conn_string)
+    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True)
     cursor = db.cursor()
     sql = "UPDATE options SET value='"+opt_val+"' WHERE name='"+opt_name+"';"
     cursor.execute(sql)
@@ -30,7 +29,7 @@ def set_opt(opt_name, opt_val):
 
 
 def get_opt(opt_name):
-    db = MySQLdb.connect(conn_string)
+    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True)
     cursor = db.cursor()
     sql = "SELECT value FROM options WHERE name='"+opt_name+"';"
     cursor.execute(sql)
