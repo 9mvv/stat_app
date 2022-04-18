e02a04c552270d1285aceeaaadbbb3e5a43421af
Adik <9vadim@gmail.com>
2022-04-18 23:27:19 +0300

    fix mysql port

--- db.py
+++ db.py
@@ -6,10 +6,11 @@ mysql_host = environ['DB_SERVER']
 mysql_user = environ['DB_USER']
 mysql_passwd = environ['DB_PASS']
 mysql_db = environ['DB_NAME']
+mysql_port = environ['DB_PORT']
 
 
 def add_record(sql_insert, data):
-    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True)
+    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True,port=mysql_port)
     cursor = db.cursor()
     cursor.execute(sql_insert, data)
     db.commit()
@@ -19,7 +20,7 @@ def add_record(sql_insert, data):
 
 def set_opt(opt_name, opt_val):
     log.info('Запись опции {} в БД.'.format(opt_name))
-    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True)
+    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True,port=mysql_port)
     cursor = db.cursor()
     sql = "UPDATE options SET value='"+opt_val+"' WHERE name='"+opt_name+"';"
     cursor.execute(sql)
@@ -29,7 +30,7 @@ def set_opt(opt_name, opt_val):
 
 
 def get_opt(opt_name):
-    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True)
+    db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db, charset='utf8', use_unicode=True,port=mysql_port)
     cursor = db.cursor()
     sql = "SELECT value FROM options WHERE name='"+opt_name+"';"
     cursor.execute(sql)
