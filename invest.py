84b61b805280b24ce84e302f6cb0b8d525125465
Adik <9vadim@gmail.com>
2022-05-16 19:50:00 +0300

    invest balance wihout sdk

--- invest.py
+++ invest.py
@@ -1,16 +1,13 @@
-import requests
-import log
 import db
+import requests
+import json
 from os import environ
-from tinkoff.invest import Client
-
 import tgl
-
-CONTRACT_PREFIX = "tinkoff.public.invest.api.contract.v1."
 invest_token = environ['INVEST_TOKEN']
 
-
+#получение баласна с использованием офф SDK
 def get_invest_balance():
+    from tinkoff.invest import Client
     try:
         TOKEN = invest_token
         with Client(TOKEN) as client:
@@ -25,3 +22,36 @@ def get_invest_balance():
             db.set_invest_balance('3', str(brok_balance+iis_balance))
     except:
         tgl.send("Ошибка обновления баланса инвестиций")
+
+#получение баласна без SDK
+def get_invest_balance2():
+    # Получение баланса Брок счет 2003351141
+    response_brok = get_invest_request("2003351141").json()
+    print(response_brok)
+    brok_balance=int(response_brok["totalAmountShares"]["units"])+ \
+        int(response_brok["totalAmountCurrencies"]["units"])+ \
+        int(response_brok["totalAmountEtf"]["units"])+ \
+        int(response_brok["totalAmountFutures"]["units"])
+    print("Общий:" + str(brok_balance))
+    # Получение баланса ИИС 2019998208
+    response_iis = get_invest_request("2019998208").json()
+    print(response_iis)
+    iis_balance = int(response_iis["totalAmountShares"]["units"])+ \
+        int(response_iis["totalAmountCurrencies"]["units"])+ \
+        int(response_iis["totalAmountEtf"]["units"])+ \
+        int(response_iis["totalAmountFutures"]["units"])
+    print("Общий:" + str(iis_balance))
+    #PortfolioResponse = client.operations.get_portfolio(account_id='2019998208')
+    db.set_invest_balance('3', str(brok_balance+iis_balance))
+
+def get_invest_request(account):
+    url = "https://invest-public-api.tinkoff.ru/rest/tinkoff.public.invest.api.contract.v1.OperationsService/GetPortfolio"
+    payload = json.dumps({
+      "accountId": account
+    })
+    headers = {
+      'Authorization': 'Bearer '+invest_token,
+      'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import time
 import log
 import wot
 import weather
-#import invest
+import invest
+
 
 from os import environ
-print(environ)
 
 log.info('Start application')
 #Получение инфы при старте
-#invest.get_invest_balance()
+invest.get_invest_balance2()
 weather.get_data()
 wot.add_record()
 wot.update_token()
@@ -18,7 +18,7 @@ wot.update_token()
 schedule.every().hour.do(wot.add_record)
 schedule.every().hour.do(weather.get_data)
 schedule.every().day.at("00:00").do(wot.update_token)
-#schedule.every().hour.do(invest.get_invest_balance)
+schedule.every().hour.do(invest.get_invest_balance2)
 
 while True:
     schedule.run_pending()
